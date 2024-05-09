4b52c1d02842cf87f29b13af6460a066e879e4a8
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-09 19:50:56 +1000

    kaupenCommitNine

--- src/main/java/wiiu/mavity/kaupen_mod/mixin/ExampleMixin.java
+++ src/main/java/wiiu/mavity/kaupen_mod/mixin/ExampleMixin.java
@@ -1,9 +1,9 @@
 package wiiu.mavity.kaupen_mod.mixin;
 
 import net.minecraft.server.MinecraftServer;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
