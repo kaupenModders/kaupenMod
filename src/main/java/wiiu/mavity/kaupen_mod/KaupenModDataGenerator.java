d9bc7190089f5ab359c0394ac6952ff434d2e7d3
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-09 20:02:34 +1000

    kaupenCommitTen

--- src/main/java/wiiu/mavity/kaupen_mod/KaupenModDataGenerator.java
+++ src/main/java/wiiu/mavity/kaupen_mod/KaupenModDataGenerator.java
@@ -1,11 +1,10 @@
 package wiiu.mavity.kaupen_mod;
 
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.*;
 
 public class KaupenModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		// Ew datagen
 	}
 }
