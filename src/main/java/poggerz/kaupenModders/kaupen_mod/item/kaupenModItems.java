60c34e0fe11f1a09e0d259d3af8179e700e702aa
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-16 19:53:45 +1000

    no.

--- src/main/java/poggerz/kaupenModders/kaupen_mod/item/kaupenModItems.java
+++ src/main/java/poggerz/kaupenModders/kaupen_mod/item/kaupenModItems.java
@@ -5,11 +5,14 @@ import net.minecraft.item.PickaxeItem;
 
 import poggerz.kaupenModders.kaupen_mod.kaupenMod;
 
+import wiiu.mavity.mavity_lib.cosmeticstuff.common.item.ClothingItem;
 import wiiu.mavity.mavity_lib.item.ItemRegistry;
 import wiiu.mavity.mavity_lib.property.PropertyInit;
 
 public class kaupenModItems {
     public static final Item kaupenPickaxe = ItemRegistry.registerItem(kaupenMod.MOD_ID, "kaupen_pickaxe", new PickaxeItem(kaupenModToolMaterial.KAUPEN, 2, 2f, PropertyInit.GTOI));
+
+    public static final Item kaupenCosplay = ItemRegistry.registerItem(kaupenMod.MOD_ID, "kaupen_cosplay", new ClothingItem(PropertyInit.GTOIU, "kaupen_cosplay"));
 
     public static void registerKaupenModItems() {
         kaupenMod.LOGGER.info(kaupenMod.NAME + " has registered its items.");
